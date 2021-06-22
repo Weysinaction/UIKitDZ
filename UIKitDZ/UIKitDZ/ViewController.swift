7574a563cf39f96b093cedf6be8c1cc2d7bf5092
[Weysinaction] <[lipatoff.const@gmail.com]>
2021-06-22 14:41:31 +0300

    Проработал код из урока 3

--- UIKitDZ/UIKitDZ/ViewController.swift
+++ UIKitDZ/UIKitDZ/ViewController.swift
@@ -10,11 +10,32 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet var hiLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func showButton(_ sender: Any) {
+        self.hiLabel.text = "Hi, "
+        self.alert(title: "Внимание", message: "Введите ваше имя", style: .alert)
+    }
+    
+    //Second var
+    func alert(title: String, message: String, style: UIAlertController.Style) {
+        let alertController = UIAlertController.init(title: title, message: message, preferredStyle: style)
+        let action = UIAlertAction(title: "ok", style: .cancel) { (action) in
+            let text = alertController.textFields?.first
+            self.hiLabel.text! += (text?.text)! + ("!")
+        }
+        
+        alertController.addTextField { (textField) in
+            textField.isSecureTextEntry = true
+        }
+        alertController.addAction(action)
+        self.present(alertController, animated: true, completion: nil)
+    }
+    
 }
 
