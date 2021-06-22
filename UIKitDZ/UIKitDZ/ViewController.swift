1e27dfab3eed6b2b754b241df889ae49aee838be
[Weysinaction] <[lipatoff.const@gmail.com]>
2021-06-22 17:35:52 +0300

    сделал дз 3 , пункты 1-4.

--- UIKitDZ/UIKitDZ/ViewController.swift
+++ UIKitDZ/UIKitDZ/ViewController.swift
@@ -8,13 +8,110 @@
 
 import UIKit
 
+let buttonSum = UIButton(type: .system)
+let buttonChoose = UIButton(type: .system)
+var labelName = UILabel()
+
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
+        let buttonSumX = 65
+        let buttonSumY = 350
+        let buttonWidth = 100
+        let buttonHeight = 50
+        
+        let buttonChooseX = 215
+        let buttonChooseY = 350
+        
+        let labelNameX = 15
+        let labelNameY = 150
+        let labelNameWidth = 350
+        let labelNameHeight = 50
+        
+        labelName = UILabel(frame: CGRect(x: labelNameX, y: labelNameY, width: labelNameWidth, height: labelNameHeight))
+        
+        buttonSum.setTitle("Сложение", for: .normal)
+        buttonSum.tintColor = .white
+        buttonSum.backgroundColor = .red
+        buttonSum.addTarget(self, action: #selector(buttonSumClicked), for: .touchUpInside)
+        
+        buttonChoose.setTitle("Угадай число", for: .normal)
+        buttonChoose.tintColor = .white
+        buttonChoose.backgroundColor = .green
+        buttonChoose.addTarget(self, action: #selector(buttonChooseClicked), for: .touchUpInside)
+        
+        labelName.textAlignment = .center
+        labelName.text = "Введите ФИО"
+        
+        buttonSum.frame = CGRect(x: buttonSumX, y: buttonSumY, width: buttonWidth, height: buttonHeight)
+        buttonChoose.frame = CGRect(x: buttonChooseX, y: buttonChooseY, width: buttonWidth, height: buttonHeight)
+        
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        
+        self.view.addSubview(buttonSum)
+        self.view.addSubview(buttonChoose)
+        self.view.addSubview(labelName)
+    }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        let alertController = UIAlertController(title: "ФИО", message: "Введите ФИО", preferredStyle: .alert)
+        let action = UIAlertAction(title: "Готово", style: .cancel) { (action) in
+            
+            labelName.text = alertController.textFields?.first?.text
+            
+        }
+        alertController.addTextField(configurationHandler: nil)
+        alertController.addAction(action)
+        
+        self.present(alertController, animated: true, completion: nil)
     }
 
+    @objc func buttonSumClicked (sender: UIButton) {
+        let alert = UIAlertController(title: "Сложение", message: "Введите числа:", preferredStyle: .alert)
+        let action = UIAlertAction(title: "Посчитать", style: .cancel) { (action) in
+            var summary: Int? = 0
+            
+            if alert.textFields?.first?.text != nil && alert.textFields?.last != nil {
+                summary = Int((alert.textFields?.first?.text)!)! + Int((alert.textFields?.last?.text)!)!
+            }
+            
+            let alertResult = UIAlertController(title: "Результат сложения", message: "\((alert.textFields?.last?.text) ?? "") + \((alert.textFields?.first?.text)!) = \(summary!)", preferredStyle: .actionSheet)
+            alertResult.addAction(UIAlertAction(title: "Ok", style: .cancel, handler: nil))
+            self.present(alertResult, animated: true) {}
+            
+        }
+        alert.addTextField(configurationHandler: nil)
+        alert.addTextField(configurationHandler: nil)
+        alert.addAction(action)
+        self.present(alert, animated: true) {
+            
+        }
+    }
+    
+    @objc func buttonChooseClicked (sender: UIButton) {
+        let alert = UIAlertController(title: "А ну-ка угадай-ка!", message: "Отгадайте число от 1 до 10", preferredStyle: .alert)
+        let action = UIAlertAction(title: "Узнать результат", style: .cancel) { (action) in
+            let randomNum = Int.random(in: 0...10)
+            var resString = "Вы не ввели значения"
+            if let number = alert.textFields?.first?.text {
+                if Int(number) == randomNum {
+                    resString = "Поздравляем, вы угадали число!"
+                } else {
+                    resString = "К сожалению, вы не угадали число, правильный ответ - \(randomNum)"
+                }
+            }
+            let alertResult = UIAlertController(title: "Результат", message: resString, preferredStyle: .alert)
+            alertResult.addAction(UIAlertAction(title: "Ok", style: .cancel, handler: nil))
+            self.present(alertResult, animated: true) {}
+        }
+            
+        alert.addTextField(configurationHandler: nil)
+        alert.addAction(action)
+        self.present(alert, animated: true) {
+            
+        }
+    }
 
 }
 
