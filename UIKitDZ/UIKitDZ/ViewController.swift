3c6df4d5ccc76212632773a02da41f97176295b2
[Weysinaction] <[lipatoff.const@gmail.com]>
2021-06-29 04:25:44 +0300

    Попрактиковался с уроком из видео, начал улучшать приложение в рамках ДЗ.

--- UIKitDZ/UIKitDZ/ViewController.swift
+++ UIKitDZ/UIKitDZ/ViewController.swift
@@ -10,11 +10,62 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    var segmentControl = UISegmentedControl()
+    var imageView = UIImageView()
+    var buttonSend = UIButton()
+    var menuArray = ["Обувь", "Шорты", "Футболки"]
+    var imageArray = [UIImage(named: "iu-8"),
+                     UIImage(named: "iu-9"),
+                     UIImage(named: "iu-10"),
+                     UIImage(named: "iu-11"),
+                     UIImage(named: "iu-12"),
+                     UIImage(named: "iu-13"),
+                     UIImage(named: "iu-14"),
+                     UIImage(named: "iu-15"),
+                     UIImage(named: "iu-16"),
+                     UIImage(named: "iu-17"),
+                     UIImage(named: "iu-18"),
+                     UIImage(named: "iu-19")]
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        
+        //create segment
+        self.segmentControl = UISegmentedControl(items: self.menuArray)
+        self.segmentControl.frame = CGRect(x: 100, y: 700, width: 200, height: 30)
+        self.view.addSubview(self.segmentControl)
+        
+        //create view
+        self.imageView.frame = CGRect(x: 0, y: 0, width: 300, height: 300)
+        self.imageView.center = self.view.center
+        self.imageView.image = self.imageArray[0]
+        self.view.addSubview(self.imageView)
+        
+        //create button
+        self.buttonSend.frame = CGRect(x: 100, y: 800, width: 100, height: 50)
+        //self.buttonSend.setTitle("Send")
+        self.buttonSend.addTarget(self, action: #selector(sendPressed), for: .touchUpInside)
+        self.view.addSubview(buttonSend)
+        
+        
+        self.segmentControl.addTarget(self, action: #selector(changeSegment), for: .valueChanged)
     }
 
+    @objc func changeSegment (target: UISegmentedControl) {
+        if target == segmentControl {
+            let segmentIndex = target.selectedSegmentIndex
+            
+            self.imageView.image = self.imageArray[segmentIndex]
+            
+            //let pr = target.titleForSegment(at: segmentIndex)
+            
+        }
+    }
+    
+    @objc func sendPressed (sender: UIButton) {
+        
+    }
 
 }
 
