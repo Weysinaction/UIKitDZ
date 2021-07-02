4063d317cd13c29bc1229305c7486924dc56cc45
[Weysinaction] <[lipatoff.const@gmail.com]>
2021-07-02 03:20:33 +0300

    Revert "Попрактиковался по видео"
    This reverts commit ac4661840adb1f5dc22efd502759ec8b510017c8.

--- UIKitDZ/UIKitDZ/SceneDelegate.swift
+++ UIKitDZ/UIKitDZ/SceneDelegate.swift
@@ -11,7 +11,6 @@ import UIKit
 class SceneDelegate: UIResponder, UIWindowSceneDelegate {
 
     var window: UIWindow?
-    var navController = UINavigationController()
 
 
     func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
@@ -19,16 +18,6 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
         // If using a storyboard, the `window` property will automatically be initialized and attached to the scene.
         // This delegate does not imply the connecting scene or session are new (see `application:configurationForConnectingSceneSession` instead).
         guard let _ = (scene as? UIWindowScene) else { return }
-        
-        
-        let viewController = ViewController()
-        self.navController = UINavigationController(rootViewController: viewController)
-        
-        self.window?.rootViewController = self.navController
-        
-        self.window?.backgroundColor = .white
-        self.window?.makeKeyAndVisible()
-        
     }
 
     func sceneDidDisconnect(_ scene: UIScene) {
--- UIKitDZ/UIKitDZ/ViewController.swift
+++ UIKitDZ/UIKitDZ/ViewController.swift
@@ -9,80 +9,12 @@
 import UIKit
 
 class ViewController: UIViewController {
-    
-//    var displaySecondButton = UIButton()
-    
+
     override func viewDidLoad() {
         super.viewDidLoad()
-        
-        self.title = "View Controller"
-        
-        let items = ["up", "down"]
-        
-        let segmentController = UISegmentedControl(items: items)
-        segmentController.isMomentary = true
-        segmentController.addTarget(self, action: #selector(segmentTapped(param:)), for: .valueChanged)
-        
-        
-        self.navigationItem.rightBarButtonItem = UIBarButtonItem(barButtonSystemItem: .camera,
-                                                                 target: self,
-                                                                 action: #selector(performAdd(sender:)))
-
-        let simpleSwitch = UISwitch()
-        simpleSwitch.isOn = true
-        simpleSwitch.addTarget(self, action: #selector(valueSwitched(param:)), for: .valueChanged)
-        
-        self.navigationItem.leftBarButtonItem = UIBarButtonItem(customView: simpleSwitch)
-        self.navigationItem.rightBarButtonItem = UIBarButtonItem(customView: segmentController)
-        
-        
-        
-//        self.displaySecondButton = UIButton(type: .system)
-//        self.displaySecondButton.setTitle("SecondVC", for: .normal)
-//        self.displaySecondButton.sizeToFit()
-//        self.displaySecondButton.center = self.view.center
-//        self.displaySecondButton.addTarget(self, action: #selector(displaySecondButtonTapped), for: .touchUpInside)
-//        self.view.addSubview(displaySecondButton)
-    }
-    
-    @objc func segmentTapped(param: UISegmentedControl) {
-        switch param.selectedSegmentIndex {
-        case 0:
-            print("up")
-        case 1:
-            print("down")
-        default:
-            break
-        }
-    }
-    @objc func valueSwitched(param: UISwitch) {
-        if param.isOn {
-            print("isOn")
-        } else {
-            print("isOff")
-        }
-    }
-    
-    @objc func performAdd(sender: UIBarButtonItem) {
-        print("add click")
-    }
-    //MARK: Method
-    
-    func createImageTitleView() {
-        let imageView = UIImageView(frame: CGRect(x: 0, y: 0, width: 24, height: 24))
-        imageView.contentMode = .scaleAspectFill
-        
-        let image = UIImage(named: "apple")
-        
-        imageView.image = image
-        
-        self.navigationItem.titleView = imageView
+        // Do any additional setup after loading the view.
     }
 
-//    @objc func displaySecondButtonTapped(sender: Any) {
-//        let vc = SecondViewController()
-//        self.navigationController?.pushViewController(vc, animated: true)
-//    }
 
 }
 
