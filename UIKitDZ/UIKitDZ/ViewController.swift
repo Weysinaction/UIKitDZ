d73c30e03194cfa8e73e464fbd083cb7abd7cea1
[Weysinaction] <[lipatoff.const@gmail.com]>
2021-07-02 03:28:36 +0300

    sync

---
+++ UIKitDZ/UIKitDZ/ViewController.swift
@@ -0,0 +1,21 @@
+//
+//  ViewController.swift
+//  UIKitDZ
+//
+//  Created by Владислав Лазарев on 22.06.2021.
+//  Copyright © 2021 Владислав Лазарев. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        
+    }
+
+
+}
+
