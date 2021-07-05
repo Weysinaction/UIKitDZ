3862ffd6e25b7c0d6f41fb42348f973f247663b9
[Weysinaction] <[lipatoff.const@gmail.com]>
2021-07-05 21:32:32 +0300

    Revert "Попрактиковался с кодом из видео"
    This reverts commit 4edc0d2934d88d1aa9420b4dd4e4fa2f5afd685b.

--- UIKitDZ/UIKitDZ/ViewController.swift
+++ UIKitDZ/UIKitDZ/ViewController.swift
@@ -10,25 +10,9 @@ import UIKit
 
 class ViewController: UIViewController {
 
-    let myLabel = UILabel()
-    
     override func viewDidLoad() {
         super.viewDidLoad()
-        
-        let labelFrame = CGRect(x: 0, y: 0, width: 100, height: 50)
-        
-        myLabel.frame = labelFrame
-        myLabel.numberOfLines = 0
-        myLabel.lineBreakMode = .byWordWrapping
-        myLabel.text = "The swift developers"
-        myLabel.font = UIFont.boldSystemFont(ofSize: 24)
-        myLabel.adjustsFontSizeToFitWidth = true
-        myLabel.sizeToFit()
-        myLabel.textColor = .black
-        myLabel.shadowColor = .lightGray
-        myLabel.shadowOffset = CGSize(width: 4 , height: 4)
-        myLabel.center = self.view.center
-        self.view.addSubview(myLabel)
+        // Do any additional setup after loading the view.
     }
 
 
