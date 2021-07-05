4edc0d2934d88d1aa9420b4dd4e4fa2f5afd685b
[Weysinaction] <[lipatoff.const@gmail.com]>
2021-07-05 21:32:14 +0300

    Попрактиковался с кодом из видео

--- UIKitDZ/UIKitDZ/ViewController.swift
+++ UIKitDZ/UIKitDZ/ViewController.swift
@@ -10,9 +10,25 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    let myLabel = UILabel()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        let labelFrame = CGRect(x: 0, y: 0, width: 100, height: 50)
+        
+        myLabel.frame = labelFrame
+        myLabel.numberOfLines = 0
+        myLabel.lineBreakMode = .byWordWrapping
+        myLabel.text = "The swift developers"
+        myLabel.font = UIFont.boldSystemFont(ofSize: 24)
+        myLabel.adjustsFontSizeToFitWidth = true
+        myLabel.sizeToFit()
+        myLabel.textColor = .black
+        myLabel.shadowColor = .lightGray
+        myLabel.shadowOffset = CGSize(width: 4 , height: 4)
+        myLabel.center = self.view.center
+        self.view.addSubview(myLabel)
     }
 
 
