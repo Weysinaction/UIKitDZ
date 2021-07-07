f87dce4f276fa4ffee6a6a660905237b2c722440
[Weysinaction] <[lipatoff.const@gmail.com]>
2021-07-07 12:47:21 +0300

    Практика по видео-уроку

--- UIKitDZ/UIKitDZ/ViewController.swift
+++ UIKitDZ/UIKitDZ/ViewController.swift
@@ -8,13 +8,38 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITextFieldDelegate {
 
+    var myTextField = UITextField()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        createTextField()
+        myTextField.delegate = self
+        
+        NotificationCenter.default.addObserver(forName: NSNotification.Name., object: <#T##Any?#>, queue: <#T##OperationQueue?#>, using: <#T##(Notification) -> Void#>)
     }
 
-
+    //MARK: CreateUI
+    
+    func createTextField() {
+        let textFieldFrame = CGRect(x: 0, y: 0, width: 200, height: 31)
+        
+        myTextField = UITextField(frame: textFieldFrame)
+        myTextField.borderStyle = UITextField.BorderStyle.roundedRect
+        myTextField.contentVerticalAlignment = UIControl.ContentVerticalAlignment.center
+        myTextField.placeholder = "I swift developer"
+        myTextField.center = self.view.center
+        self.view.addSubview(self.myTextField)
+    }
+    
+    //Mark: -UITextFieldDelegate
+    
+    
+    //MARK: Notification
+    @objc func textFieldTextDidChange(ncParam: NSNotification) {
+        print("UITextFieldTextDidChange = \(ncParam)")
+    }
 }
 
