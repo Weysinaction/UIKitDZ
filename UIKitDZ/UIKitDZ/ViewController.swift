77c1c1256d073d1deb9566a255db73171e5d358c
[Weysinaction] <[lipatoff.const@gmail.com]>
2021-07-09 20:30:05 +0300

    Revert "попрактиковался с кодом из видео"
    This reverts commit 5a76999d035ff55925e4171946c7b963ee0c5756.

--- UIKitDZ/UIKitDZ/ViewController.swift
+++ UIKitDZ/UIKitDZ/ViewController.swift
@@ -10,42 +10,11 @@ import UIKit
 
 class ViewController: UIViewController {
 
-    var myTextView = UITextView()
     override func viewDidLoad() {
         super.viewDidLoad()
-        
-        createTextView()
-        NotificationCenter.default.addObserver(self, selector: #selector(updateTextView(param:)), name: UIResponder.keyboardDidShowNotification, object: nil)
-        NotificationCenter.default.addObserver(self, selector: #selector(updateTextView(param:)), name: UIResponder.keyboardWillHideNotification, object: nil)
+        // Do any additional setup after loading the view.
     }
 
 
-    func createTextView() {
-        myTextView = UITextView(frame: CGRect(x: 20, y: 100, width: self.view.bounds.width - 40, height: self.view.bounds.height / 2))
-        myTextView.text = "Some text"
-        myTextView.contentInset = UIEdgeInsets(top: 50, left: 0, bottom: 0, right: 0)
-        myTextView.backgroundColor = .gray
-        myTextView.font = UIFont.systemFont(ofSize: 17)
-        self.view.addSubview(myTextView)
-    }
-    
-    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
-        self.myTextView.resignFirstResponder()
-    }
-    
-    @objc func updateTextView(param: Notification) {
-        let userInfo = param.userInfo
-        
-        let getKeyboardRect = (userInfo![UIResponder.keyboardFrameEndUserInfoKey] as! NSValue).cgRectValue
-        let keyboardFrame = self.view.convert(getKeyboardRect, to: view.window)
-        
-        if param.name == UIResponder.keyboardWillHideNotification {
-            myTextView.contentInset = UIEdgeInsets.zero
-        } else {
-            myTextView.contentInset = UIEdgeInsets(top: 0, left: 0, bottom: keyboardFrame.height, right: 0)
-            myTextView.scrollIndicatorInsets = myTextView.contentInset
-        }
-        myTextView.scrollRangeToVisible(myTextView.selectedRange)
-    }
 }
 
