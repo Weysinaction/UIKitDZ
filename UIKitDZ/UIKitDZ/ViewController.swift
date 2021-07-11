3f732f6725f43281970d643fa2dc65356756248b
[Weysinaction] <[lipatoff.const@gmail.com]>
2021-07-11 17:50:36 +0300

    Попрактиковался с кодом из видео

--- UIKitDZ/UIKitDZ/ViewController.swift
+++ UIKitDZ/UIKitDZ/ViewController.swift
@@ -9,12 +9,33 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    var myButton = UIButton()
+    let normalImage = UIImage(systemName: "person.fill")
+    let pressedImage = UIImage(systemName: "alarm")
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        myButton = UIButton(type: .roundedRect)
+        myButton.frame = CGRect(x: 0, y: 0, width: 200, height: 200)
+        myButton.setTitle("1", for: .normal)
+        myButton.setTitle("2", for: .highlighted)
+        myButton.center = self.view.center
+        myButton.addTarget(self, action: #selector(buttonIsPressed(sender:)), for: .touchDown)
+        myButton.addTarget(self, action: #selector(buttonIsTapped(sender:)), for: .touchUpInside)
+        myButton.setBackgroundImage(normalImage, for: .normal)
+        myButton.setBackgroundImage(pressedImage, for: .highlighted)
+        self.view.addSubview(myButton)
     }
 
-
+    
+    @objc func buttonIsPressed(sender: UIButton) {
+        print("Button is pressed")
+    }
+    
+    @objc func buttonIsTapped(sender: UIButton) {
+        print("Button is tapped")
+    }
 }
 
